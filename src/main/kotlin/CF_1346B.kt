084537aafeaad60afb8d70eb1887463ce43777fe
조재국 <hoxymola@gmail.com>
2022-03-28 09:14:19 +0900

    2022-03-28

---
+++ src/main/kotlin/CF_1346B.kt
@@ -0,0 +1,16 @@
+/**
+ * @author Jaeguk Cho
+ */
+
+fun main() = repeat(readLine()!!.toInt()) {
+    val (n, k1, k2) = readLine()!!.split(' ').map { it.toInt() }
+    var ans = 0
+    var prev = 0
+    readLine()!!.forEach {
+        if (it == '1') {
+            prev = minOf(k1, k2 - prev)
+            ans += prev
+        } else prev = 0
+    }
+    println(ans)
+}
