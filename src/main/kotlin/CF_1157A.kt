1337e87674dafba77b76535641e9590f3f9bd760
조재국 <hoxymola@gmail.com>
2022-04-09 20:30:24 +0900

    2022-04-09 - HashSet::add(), dropLastWhile { }

--- src/main/kotlin/CF_1157A.kt
+++ src/main/kotlin/CF_1157A.kt
@@ -5,8 +5,8 @@
 fun main() {
     val reached = hashSetOf<Int>()
     var n = readln().toInt()
-    while (reached.add(n)) n = f(n)
+    while (reached.add(n)) n = n.next()
     println(reached.size)
 }
 
-fun f(n: Int) = (n + 1).toString().dropLastWhile { it == '0' }.toInt()
+fun Int.next() = (this + 1).toString().dropLastWhile { it == '0' }.toInt()
