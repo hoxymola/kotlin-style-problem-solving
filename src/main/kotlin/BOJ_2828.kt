5c4aa4fed5169fdd6a0769943ffb465db22ade61
조재국 <hoxymola@gmail.com>
2022-04-09 23:36:51 +0900

    2022-03-22

--- src/main/kotlin/BOJ_2828.kt
+++ src/main/kotlin/BOJ_2828.kt
@@ -1,15 +1,16 @@
 /**
  * @author Jaeguk Cho
  */
+
 fun main() {
-    val (n, m) = readLine()!!.split(' ').map { it.toInt() }
-    val j = readLine()!!.toInt()
+    val (n, m) = readln().split(' ').map { it.toInt() }
+    val j = readln().toInt()
     var l = 1
     var r: Int
     var ans = 0
     for (i in 1..j) {
         r = l + m - 1
-        when (val apple = readLine()!!.toInt()) {
+        when (val apple = readln().toInt()) {
             in 1 until l -> {
                 ans += l - apple
                 l = apple
