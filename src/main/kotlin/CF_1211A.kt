e2ef7775ef32740a80191c8816b5ce65813bba58
조재국 <hoxymola@gmail.com>
2022-04-09 23:18:54 +0900

    2022-04-09 - asSequence(), withIndex(), distinctBy { },

--- src/main/kotlin/CF_1211A.kt
+++ src/main/kotlin/CF_1211A.kt
@@ -3,10 +3,18 @@
  */
 
 fun main() {
-    readLine()
-    readLine()!!.split(' ').map { it.toInt() }
+    readln()
+    readln().split(' ').asSequence()
+        .map { it.toInt() }
         .withIndex()
         .distinctBy { it.value }
         .sortedBy { it.value }
         .map { it.index + 1 }
+        .toList()
+        .also {
+            println(
+                if (it.size < 3) "-1 -1 -1"
+                else "${it[0]} ${it[1]} ${it[2]}"
+            )
+        }
 }
