f8c99f7a5c1b8cf4c93239027015caeffaac85ee
조재국 <hoxymola@gmail.com>
2022-04-09 23:37:34 +0900

    2022-03-22 - generateSequence() { }

--- src/main/kotlin/BOJ_9625.kt
+++ src/main/kotlin/BOJ_9625.kt
@@ -1,7 +1,8 @@
 /**
  * @author Jaeguk Cho
  */
+
 fun main() {
-    val fib = generateSequence(Pair(0, 1)) { Pair(it.second, it.first + it.second) }.elementAt(readLine()!!.toInt() - 1)
+    val fib = generateSequence(Pair(0, 1)) { Pair(it.second, it.first + it.second) }.elementAt(readln().toInt() - 1)
     println("${fib.first} ${fib.second}")
 }
