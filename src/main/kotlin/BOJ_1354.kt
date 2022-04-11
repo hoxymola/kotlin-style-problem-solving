0f9c768d8d1f4906616b224214dd6f6f0d5e7c3b
조재국 <hoxymola@gmail.com>
2022-04-12 02:08:22 +0900

    2022-04-12

---
+++ src/main/kotlin/BOJ_1354.kt
@@ -0,0 +1,10 @@
+/**
+ * @author Jaeguk Cho
+ */
+
+fun main() {
+    val cache = hashMapOf<Long, Long>()
+    val (n, p, q, x, y) = readln().split(' ').map { it.toLong() }
+    fun a(n: Long): Long = if (n <= 0) 1 else cache.getOrPut(n) { a(n / p - x) + a(n / q - y) }
+    println(a(n))
+}
