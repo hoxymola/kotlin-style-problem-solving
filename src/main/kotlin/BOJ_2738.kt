dc70e149913ea84046684063eb0909898fa730fd
조재국 <hoxymola@gmail.com>
2022-04-29 16:43:14 +0900

    2022-04-29 - zip(), flatten(), chunked()

--- src/main/kotlin/BOJ_2738.kt
+++ src/main/kotlin/BOJ_2738.kt
@@ -3,10 +3,9 @@
  */
 
 fun main() {
-    operator fun List<Int>.plus(list: List<Int>) =
-        mapIndexed { i, v -> v + list[i] }
-
     val (n, m) = readln().split(' ').map { it.toInt() }
     val (a, b) = List(2) { List(n) { readln().split(' ').map { it.toInt() } }.flatten() }
-    println((a + b).chunked(m).joinToString("\n") { it.joinToString(" ") })
+    println(
+        a.zip(b).map { it.first + it.second }.chunked(m).joinToString("\n") { it.joinToString(" ") }
+    )
 }
