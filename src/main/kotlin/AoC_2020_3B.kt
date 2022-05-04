62a9d915777c3a8693cc3d8fc9a997f01c85e48e
조재국 <hoxymola@gmail.com>
2022-05-05 01:49:37 +0900

    2022-05-05 - reduce()

---
+++ src/main/kotlin/AoC_2020_3B.kt
@@ -0,0 +1,22 @@
+import java.io.File
+
+/**
+ * @author Jaeguk Cho
+ */
+
+fun List<String>.count(a: Int, b: Int, c: Int): Long =
+    mapIndexed { i, v ->
+        i % a == 0 && v[i * b / c % v.length] == '#'
+    }.count { it }.toLong()
+
+fun main() = println(
+    with(File("src/main/kotlin/input").readLines()) {
+        listOf(
+            count(1, 1, 1),
+            count(1, 3, 1),
+            count(1, 5, 1),
+            count(1, 7, 1),
+            count(2, 1, 2)
+        ).reduce(Long::times)
+    }
+)
