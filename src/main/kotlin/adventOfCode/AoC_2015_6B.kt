c575f5837867329a2b65bd798e661cdda3cfa13f
조재국 <hoxymola@gmail.com>
2022-05-08 02:49:03 +0900

    2022-05-08

--- src/main/kotlin/adventOfCode/AoC_2015_6B.kt
+++ src/main/kotlin/adventOfCode/AoC_2015_6B.kt
@@ -7,15 +7,15 @@ package adventOfCode
 fun main() {
     val lights = List(1000) { MutableList(1000) { 0 } }
 
-    fun List<MutableList<Int>>.adjust(line: String, amount: Int) {
-        val (y1, x1, y2, x2) = line.split(" through ", ",").map { it.toInt() }
+    fun adjust(range: String, amount: Int) {
+        val (y1, x1, y2, x2) = range.split(" through ", ",").map { it.toInt() }
 
         for (i in y1..y2)
             for (j in x1..x2)
-                this[i][j] = (this[i][j] + amount).takeIf { it > 0 } ?: 0
+                lights[i][j] = (lights[i][j] + amount).takeIf { it > 0 } ?: 0
     }
 
-    fun List<MutableList<Int>>.operation(line: String) {
+    fun operation(line: String) {
         when (line.substringAfter(' ').substringBefore(' ')) {
             "on" -> adjust(line.substringAfter(' ').substringAfter(' '), 1)
             "off" -> adjust(line.substringAfter(' ').substringAfter(' '), -1)
@@ -23,6 +23,6 @@ fun main() {
         }
     }
 
-    readLines("2015_06").forEach { lights.operation(it) }
+    readLines("2015_06").forEach { operation(it) }
     println(lights.sumOf { it.sum() })
 }
