542727e374cd013e3df998039da976a2b60d1ecd
조재국 <hoxymola@gmail.com>
2022-05-10 18:07:03 +0900

    2022-05-10

---
+++ src/main/kotlin/adventOfCode/AoC_2016_04A.kt
@@ -0,0 +1,18 @@
+package adventOfCode
+
+/**
+ * @author Jaeguk Cho
+ */
+
+fun main() {
+    println(
+        readLines("2016_04").sumOf {
+            if (it.substringBeforeLast('-').filter { it != '-' }.toCharArray().groupBy { it }
+                    .map { it.key to it.value.size }.sortedWith { p, q ->
+                        if (p.second != q.second) q.second - p.second
+                        else p.first - q.first
+                    }.map { it.first }.joinToString("").take(5) == it.substringAfter('[').take(5)
+            ) it.substringAfterLast('-').substringBefore('[').toInt() else 0
+        }
+    )
+}
