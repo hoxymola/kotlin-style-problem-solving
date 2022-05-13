385c216875bbaffd489a30bc9add9344bac03acc
조재국 <hoxymola@gmail.com>
2022-05-13 13:24:20 +0900

    2022-05-13

--- src/main/kotlin/adventOfCode/AoC_2016_04A.kt
+++ src/main/kotlin/adventOfCode/AoC_2016_04A.kt
@@ -7,7 +7,7 @@ package adventOfCode
 fun main() {
     println(
         readLines("2016_04").sumOf {
-            if (it.substringBeforeLast('-').filter { it != '-' }.toCharArray().groupBy { it }
+            if (it.substringBeforeLast('-').filter { it != '-' }.groupBy { it }
                     .map { it.key to it.value.size }.sortedWith { p, q ->
                         if (p.second != q.second) q.second - p.second
                         else p.first - q.first
---
+++ src/main/kotlin/adventOfCode/AoC_2016_04B.kt
@@ -0,0 +1,17 @@
+package adventOfCode
+
+/**
+ * @author Jaeguk Cho
+ */
+
+fun main() {
+    println(
+        readLines("2016_04").map {
+            with(it.substringAfterLast('-').substringBefore('[').toInt()) {
+                it.substringBeforeLast('-').filter { it != '-' }.map {
+                    'a' + (it - 'a' + this) % 26
+                }.joinToString("") to this
+            }
+        }.first { it.first == "northpoleobjectstorage" }.second
+    )
+}
