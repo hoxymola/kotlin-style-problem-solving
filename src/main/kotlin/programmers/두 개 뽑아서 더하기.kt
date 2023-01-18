9c35838ee8381a205e39240801df464d3404417d
조재국 <hoxymola@gmail.com>
2023-01-18 22:49:40 +0900

    2023-01-17

--- src/main/kotlin/programmers/두 개 뽑아서 더하기.kt
+++ src/main/kotlin/programmers/두 개 뽑아서 더하기.kt
@@ -2,7 +2,7 @@
  * @author Jaeguk Cho
  */
 
-class Solution {
+class `두 개 뽑아서 더하기` {
     fun solution(numbers: IntArray) = numbers.flatMapIndexed { i, num ->
         numbers.drop(i + 1).map { it + num }
     }.toSortedSet()
